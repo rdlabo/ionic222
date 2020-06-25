052398579149cacfc71340ecd513bab0022a9120
mhoohmjw <67403355+mhoohmjw@users.noreply.github.com>
2020-06-25 13:03:40 +0900

    Add files via upload

---
+++ src/app/auth/signup/signup.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class SignupPage implements OnInit {
+  login: {
+    email: string;
+    password: string;
+  } = {
+    email: null,
+    password: null,
+  };
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  signUp() {
+    this.auth.authSignUp(this.login);
+  }
+}
